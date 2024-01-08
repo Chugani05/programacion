6cbaf89a1b57d66e56dc4a140b7daed17bdad343
Chugani05 <chugani205@gmail.com>
2024-01-08 16:29:06 +0000

    añadido diccionarios

---
+++ ut3/diccionarios/cities.py
@@ -0,0 +1,18 @@
+# ********************
+# DESCIFRANDO CIUDADES
+# ********************
+
+
+def run(cinfo: str) -> dict:
+    a = cinfo.split(';')
+    b = str(a).split(':')
+
+    cities = {}
+    for cinfo in 
+    cities[cinfo] = 
+
+    return cities
+
+
+if __name__ == '__main__':
+    run('Tokyo:38_140_000;Delhi:26_454_000;Shanghai:24_484_000;Mumbai:21_357_000')
