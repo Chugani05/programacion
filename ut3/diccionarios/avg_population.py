c23f336a7e96e2439a50bd17c2699001ac0a0b42
Chugani05 <chugani205@gmail.com>
2024-01-13 14:03:42 +0000

    añadiendo ejercicios

---
+++ ut3/diccionarios/avg_population.py
@@ -0,0 +1,14 @@
+# ******************
+# POBLACIÓN PROMEDIO
+# ******************
+
+
+def run(pdata: dict) -> dict:
+    # TU CÓDIGO AQUÍ
+    avg_data = 'output'
+
+    return avg_data
+
+
+if __name__ == '__main__':
+    run({'Tokyo': 38140000, 'Delhi': 26454000, 'Shanghai': 24484000, 'Mumbai': 21357000})
