59bfc666248d36c7d2e65543915be2d7c9622f3c
Chugani05 <chugani205@gmail.com>
2024-01-15 16:24:31 +0000

    añadiendo ejercicios

---
+++ ut3/diccionarios/group_words.py
@@ -0,0 +1,14 @@
+# ******************
+# AGRUPANDO PALABRAS
+# ******************
+
+
+def run(words: list) -> dict:
+    # TU CÓDIGO AQUÍ
+    group_words = 'output'
+
+    return group_words
+
+
+if __name__ == '__main__':
+    run(['mesa', 'móvil', 'barco', 'coche', 'avión', 'bandeja', 'casa', 'monitor', 'carretera', 'arco'])
---
+++ ut3/diccionarios/split_marks.py
@@ -0,0 +1,25 @@
+# *********************
+# CADA NOTA EN SU SITIO
+# *********************
+
+
+def run(marks: dict) -> tuple:
+    passed = {}
+    failed = {}
+    
+#    passed = {}
+#    failed = {}
+#    for name, mark in marks.items():
+#        if mark >= 5:
+#            passed[name.upper()] = mark
+#        else:
+#            failed[name.lower()] = mark
+
+
+
+
+    return passed, failed
+
+
+if __name__ == '__main__':
+    run({'Ana': 4, 'Domingo': 7, 'Eva': 5, 'Álvaro': 3, 'Juan': 8, 'Belén': 1})
