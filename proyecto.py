cfbdba5dffb6db1d64a33020be7a8600f70ff743
Rashi <147090292+Chugani05@users.noreply.github.com>
2024-02-22 01:01:29 +0000

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -53,6 +53,8 @@ def show_board(board: list[list[str]]) -> None:
 
 board = generate_board()
 
+# VARIABLES
+
 LETTERS = "ABCDEFGHIJ"
 TOTAL_SHIPS = 5
 game = True
@@ -65,44 +67,97 @@ b3 = []
 a2 = []
 warships = ['5A','4A','3A','3B','2A']
 
+
 game_board = [[UNEXPLORED for _ in range(10)] for _ in range(10)]
 while game:
-    player_coordinate = input("Enter your coordinates: ")
+    player_coordinate = input("Ingrese sus coordenadas: ")
     if player_coordinate[0] in LETTERS:
-        coordinate_x = LETTERS.index(player_coordinate[0].upper())
+        coordinate_y = int(LETTERS.index(player_coordinate[0].upper()))
     else:
-        coordinate_x = -1
-    coordinate_y = int(player_coordinate[1:]) - 1
-    if 0 <= coordinate_x < 10 and 0 <= coordinate_y < 10:
-        if game_board[coordinate_y][coordinate_x] == UNEXPLORED:
-            cell_content = board[coordinate_y][coordinate_x]
-            game_board[coordinate_y][coordinate_x] = TOUCHED
-            if cell_content in warships:
-                warships.remove(cell_content)
-                ship_size = int(cell_content[0])
-                if cell_content.endswith("A"):
-                    exec(f"a{ship_size}_counter += 1")
-                    if eval(f"a{ship_size}_counter") == ship_size:
-                        for touched_ship in eval(f"a{ship_size}"):
-                            game_board[LETTERS.index(touched_ship[0])][int(touched_ship[1:]) - 1] = SUNKEN
-                        points += 4 * ship_size
+        coordinate_y = 0
+    coordinate_x = int(player_coordinate[1:]) - 1
+    if coordinate_x not in range(0, 10) or coordinate_y == 0:
+        print("Por favor, introduzca una coordenada válida.")
+    else:
+        if game_board[coordinate_x][coordinate_y] == UNEXPLORED:
+            match board[coordinate_x][coordinate_y]:
+                case "5A":
+                    game_board[coordinate_x][coordinate_y] = TOUCHED
+                    a5_counter += 1
+                    points += 2 * 5
+                    a5.append(player_coordinate)
+                    if a5_counter == 5:
+                        for touched_ship in a5:
+                            game_board[int(touched_ship[1:]) - 1][LETTERS.index(touched_ship[0].upper())] = SUNKEN
+                        points += 4 * 5
+                        warships.remove("5A")
                         TOTAL_SHIPS -= 1
-                        print(f"Barco hundido, quedan {TOTAL_SHIPS} barcos")
-            else:
-                points -= 1
-                print("Has dado a una casilla de agua")
+                        notship = f"Barco hundido, quedan {TOTAL_SHIPS} barcos"
+                case "4A":
+                    game_board[coordinate_x][coordinate_y] = TOUCHED
+                    a4_counter += 1
+                    points += 2 * 4
+                    a4.append(player_coordinate)
+                    if a4_counter == 4:
+                        for touched_ship in a4:
+                            game_board[int(touched_ship[1:]) - 1][LETTERS.index(touched_ship[0].upper())] = SUNKEN
+                        points += 4 * 4
+                        warships.remove("4A")
+                        TOTAL_SHIPS -= 1
+                        notship = f"Barco hundido, quedan {TOTAL_SHIPS} barcos"
+                case "3A":
+                    game_board[coordinate_x][coordinate_y] = TOUCHED
+                    a3_counter += 1
+                    points += 2 * 3
+                    a3.append(player_coordinate)
+                    if a3_counter == 3:
+                        for touched_ship in a3:
+                            game_board[int(touched_ship[1:]) - 1][LETTERS.index(touched_ship[0].upper())] = SUNKEN
+                        points += 4 * 3
+                        warships.remove("3A")
+                        TOTAL_SHIPS -= 1
+                        notship = f"Barco hundido, quedan {TOTAL_SHIPS} barcos"
+                case "3B":
+                    game_board[coordinate_x][coordinate_y] = TOUCHED
+                    b3_counter += 1
+                    points += 2 * 3
+                    b3.append(player_coordinate)
+                    if b3_counter == 3:
+                        for touched_ship in b3:
+                            game_board[int(touched_ship[1:]) - 1][LETTERS.index(touched_ship[0].upper())] = SUNKEN
+                        points += 4 * 3
+                        warships.remove("3B")
+                        TOTAL_SHIPS -= 1
+                        notship = f"Barco hundido, quedan {TOTAL_SHIPS} barcos"
+                case "2A":
+                    game_board[coordinate_x][coordinate_y] = TOUCHED
+                    a2_counter += 1
+                    points += 2 * 2
+                    a2.append(player_coordinate)
+                    if a2_counter == 2:
+                        for touched_ship in a2:
+                            game_board[int(touched_ship[1:]) - 1][LETTERS.index(touched_ship[0].upper())] = SUNKEN
+                        points += 4 * 2
+                        warships.remove("2A")
+                        TOTAL_SHIPS -= 1
+                        notship = f"Barco hundido, quedan {TOTAL_SHIPS} barcos"
+                case _:
+                    game_board[coordinate_x][coordinate_y] = WATER
+                    notship = "Has dado a una casilla de agua"
+                    if points > 0:
+                        points -= 1
+                    else:
+                        points = 0
         else:
-            print(f"Ya has disparado a la casilla {player_coordinate}")
+            notship = f"Ya has disparado a la casilla {player_coordinate}"
         
         if TOTAL_SHIPS == 0:
             print(f"Juego terminado, tu puntuación es {points} y tardaste {turn} turnos")
             game = False
-        else:
-            print("   1  2  3  4  5  6  7  8  9 10")
-            for row in range(10):
-                row_string = " ".join(game_board[row])
-                print(f"{LETTERS[row]}  {row_string}")
-            print(f"Tu puntuación actual es: {points}, llevas {turn} rondas y quedan {len(warships)} barcos")
-            turn += 1
-    else:
-        print("Introduzca una coordenada correcta")
+        print("   1  2  3  4  5  6  7  8  9  10")
+        for row in range(10):
+            row_string = " ".join(game_board[row])
+            print(f"{LETTERS[row]}  {row_string}")
+        print(notship)
+        print(f"Tu puntuación actual es: {points}, llevas {turn} rondas y quedan {len(warships)} barcos")
+        turn += 1
