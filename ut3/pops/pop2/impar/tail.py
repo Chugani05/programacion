e3ed64d91eda1dd50e10896279ede257a6db8fe5
Chugani05 <chugani205@gmail.com>
2024-02-26 15:51:35 +0000

    actualizando

--- ut3/pops/pop2/impar/tail.py
+++ ut3/pops/pop2/impar/tail.py
@@ -5,9 +5,15 @@ from pathlib import Path
 
 
 def run(file: Path, n: int) -> str:
+    # Mi código
     with open(file, 'r') as f:
         line = f.readlines()
         lines = ''.join(line[-n:]).rstrip('\n')
+
+    # Otra forma
+    f = open(file)
+    all_lines = f.readlines()
+    lines = all_lines[-n:]
 
     return lines
 
