22374bab125067db50f9bb73e21f4efa07ea424a
Chugani05 <chugani205@gmail.com>
2024-03-15 18:37:33 +0000

    añadido

---
+++ ut4/funciones/ejemplo_exprecion_generadora.py
@@ -0,0 +1,16 @@
+def gen_sq(i: int) -> list:
+    return [i for i in range(0, 20) if i % 3 == 0]
+
+
+if __name__ == "__main__":
+    print(gen_sq(54))
+
+
+def gen_sq(i: int) -> list:
+    sq = [i for i in range(0, 20) if i % 3 == 0]
+
+    return sum(sq)
+
+
+if __name__ == "__main__":
+    print(gen_sq(54))
